6f0d2d847ccda4984b2802b6b2ca490761f19069
Leo Mihalcea <leonard.mihalcea@cloudflight.io>
2023-10-28 14:09:35 +0300

    feat(#24): regenerate ui project using vite

--- word-counter-ui/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import 'primeflex/primeflex.css'
-
-let app = createApp(App);
-app.use(PrimeVue)
-app.mount('#app')
