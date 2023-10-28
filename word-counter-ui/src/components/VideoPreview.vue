9493389d2fee1b0e5a279403fac449f1d5d84928
roxanafrunza <frunza_roxana@yahoo.com>
2023-10-28 18:42:37 +0300

    feat(#4): implement video preview component

---
+++ word-counter-ui/src/components/VideoPreview.vue
@@ -0,0 +1,28 @@
+<template>
+    <LiteYouTubeEmbed :id="youtube_parser(videoURL)" title="something?"/>
+</template>
+
+<script setup lang="ts">
+import LiteYouTubeEmbed from 'vue-lite-youtube-embed'
+import 'vue-lite-youtube-embed/style.css'
+
+const props = defineProps<{
+    videoURL: string
+}>();
+
+function youtube_parser(url: string): string{
+    if(!url) return "lADBHDg-JtA";
+
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : "";
+}
+
+</script>
+
+<style scoped>
+iframe {
+    width: 100%;
+    height: 40vh;
+}
+</style>
