d6021b2b72789f29d1ef0a09b436a92d335feb1d
roxanafrunza <frunza_roxana@yahoo.com>
2023-10-29 12:42:37 +0200

    feat(#32): implement analyse view

--- word-counter-ui/src/components/VideoPreview.vue
+++ word-counter-ui/src/components/VideoPreview.vue
@@ -1,23 +1,17 @@
 <template>
-    <LiteYouTubeEmbed :id="youtube_parser(videoURL)" title="something?"/>
+    <LiteYouTubeEmbed :id="videoStore.videoId" title="something?"/>
 </template>
 
 <script setup lang="ts">
+import { useVideoStore } from '@/stores/video';
 import LiteYouTubeEmbed from 'vue-lite-youtube-embed'
 import 'vue-lite-youtube-embed/style.css'
 
 const props = defineProps<{
-    videoURL: string
+    videoURL?: string
 }>();
 
-function youtube_parser(url: string): string{
-    if(!url) return "lADBHDg-JtA";
-
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = url.match(regExp);
-    return (match&&match[7].length==11)? match[7] : "";
-}
-
+const videoStore = useVideoStore()
 </script>
 
 <style scoped>
--- word-counter-ui/src/views/HomeView.vue
+++ word-counter-ui/src/views/HomeView.vue
@@ -8,27 +8,39 @@
             </div>
             <div class="flex flex-column gap-2">
                 <label for="url">Paste the link of a {{ selectedOption.name }}</label>
-                <InputText id="url" v-model="videoURL" placeholder="Your URL"/>
+                <InputText id="url" v-model="videoURLInput" placeholder="Your URL"/>
             </div>
-            <Button label="Count"/>
+            <Button label="Analyse" :disabled="!videoURLInput" @click="$router.push('analysis')"/>
         </div>
 
         <div class="second-column">
-            <VideoPreview :videoURL="videoURL"/>
+            <VideoPreview />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import VideoPreview from '@/components/VideoPreview.vue'
+import { useVideoStore } from '@/stores/video';
+import router from '@/router';
 
 const options = ref([
     { name: 'Playlist' },
     { name: 'Video' },
 ])
 const selectedOption = ref(options.value[0]);
-const videoURL = ref("");
+
+const videoStore = useVideoStore()
+
+const videoURLInput = computed({
+  get() {
+    return videoStore.videoURL
+  },
+  set(val) {
+    videoStore.changeVideoURL(val)
+  }
+})
 </script>
 
 <style scoped>
